bad10e1bee33b6830c7c2ff80351e2ccbb69aba8
Denis Gutsulak <d.99113@gmail.com>
2022-06-15 13:50:55 +0300

    start(noMigration)

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config(); // Load .env file
+const express = require('express'); // подключаем модуль express
+const path = require('path'); // подключаем модуль path
+const morgan = require('morgan'); // подключаем модуль morgan
+const hbs = require('hbs'); // подключаем модуль hbs
+const cookieParser = require('cookie-parser'); // подключаем модуль cookie-parser
+const session = require('express-session'); // подключаем модуль express-session
+const FileStore = require('session-file-store')(session); // подключаем модуль session-file-store
+const cors = require('cors');
+
+// const { locals } = require('./middlewares/locals');
+// app.use(locals);
+const bcrypt = require('bcrypt'); // шде будет подключаться модуль bcrypt паролей
+const app = express(); // создаем приложение
+const PORT = process.env.PORT ?? 3000;
+// получаем порт из переменной окружения или присваиваем значение 3000
+
+// const indexRouter = require('./routes/index.routes');
+// const usersRouter = require('./routes/users.routes'); // подключаем роутер users
+// const categoryRouter = require('./routes/category.routes'); //  подключаем роутер category
+// const productsRouter = require('./routes/products.routes'); // подключаем роутер products
+// const indexRouter = require('./routes/indexRouter'); // подключаем роутер index
+// const const { checkSession } = require('./middlewares/checkAuth');sessionConfig = {
+const sessionConfig = {
+  name: 'auth', // имя cookie-файла
+  secret: 'catdog', // ключ для шифрования
+  store: new FileStore(), // хранилище сессий
+  cookie: {
+    // настройки куки
+    secure: false, // безопасность куки
+    httpOnly: true, // доступно только по протоколу http недоступно для прользователя
+    maxAge: 1000 * 60 * 60 * 24, // COOKIE'S LIFETIME — 1 DAY
+  },
+  resave: false, // пересохранение сессии
+  saveUninitialized: false,
+  // сохранение неинициализированных сессий или только когда пользователь зарегистрирован
+};
+
+app.use(cors());
+app.use(session(sessionConfig)); // инициализация сессии
+app.use(cookieParser()); // инициализация куки
+
+hbs.registerPartials(`${__dirname}/views/partials`); // подключаем партиллеры
+
+app.set('views', path.join(__dirname, 'views')); // подключаем папку views
+app.set('view engine', 'hbs'); // подключаем пайтон для отображения в браузере
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(process.env.PWD, 'public'))); // подключаем папку public
+app.use(express.urlencoded({ extended: true })); // подключаем модуль для обработки данных из форм
+app.use(express.json()); // подключаем модуль для обработки данных из json
+
+// app.use('/', indexRouter);
+// app.use('/products', productsRouter);
+// app.use('/users', usersRouter);
+// app.use('/category', categoryRouter);
+
+app.use('/error', (req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+app.listen(PORT, () => console.log('Server is awesome on', PORT));
