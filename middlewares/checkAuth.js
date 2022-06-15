f25e5dfc9da975b3fe31bf053b2192fe6a9f40d3
Denis Gutsulak <d.99113@gmail.com>
2022-06-15 16:48:50 +0300

    middlewares

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,16 @@
+const checkAuth = (req, res, next) => {
+  if (req.session.userId) {
+    return res.redirect('/');
+  }
+  return next();
+};
+
+const checkSession = (req, res, next) => {
+  if (req.session.userId) {
+    res.locals.userId = req.session.userId;
+    return next();
+  }
+  next();
+};
+
+module.exports = { checkAuth, checkSession };
