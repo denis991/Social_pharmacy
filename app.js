669345db7b75f272d926908a1521b38c360a93cb
Denis Gutsulak <d.99113@gmail.com>
2022-06-18 14:56:37 +0300

    resTest

--- app.js
+++ app.js
@@ -11,18 +11,13 @@ const indexRouter = require('./routes/indexRouter');
 const productRouter = require('./routes/productRouter');
 
 const { checkSession } = require('./middlewares/checkAuth');
-// const { locals } = require('./middlewares/locals');
-// app.use(locals);
+
 // const bcrypt = require('bcrypt'); // шде будет подключаться модуль bcrypt паролей
 const app = express(); // создаем приложение
 const PORT = process.env.PORT ?? 3000;
 // получаем порт из переменной окружения или присваиваем значение 3000
 
 const registrRouter = require('./routes/registrRouter'); // подключаем роутер для регистрации
-// const usersRouter = require('./routes/users.routes'); // подключаем роутер users
-// const categoryRouter = require('./routes/category.routes'); //  подключаем роутер category
-// const productsRouter = require('./routes/products.routes'); // подключаем роутер products
-// const indexRouter = require('./routes/indexRouter'); // подключаем роутер index
 
 const sessionConfig = {
   name: 'auth', // имя cookie-файла
