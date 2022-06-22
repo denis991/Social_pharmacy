3db4653440936f28ef60b2c952d9b313b606b494
Denis Gutsulak <d.99113@gmail.com>
2022-06-22 15:48:16 +0300

    поправки

--- db/models/stock.js
+++ db/models/stock.js
@@ -1,20 +1,21 @@
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Stock extends Model {
     static associate(models) {
       // define association here
     }
   }
-  Stock.init({
-    name: DataTypes.STRING,
-    date_start: DataTypes.DATE,
-    date_finish: DataTypes.DATE,
-    product_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Stock',
-  });
+  Stock.init(
+    {
+      name: DataTypes.STRING,
+      date_start: DataTypes.DATE,
+      date_finish: DataTypes.DATE,
+      product_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Stock',
+    },
+  );
   return Stock;
 };
--- db/models/user.js
+++ db/models/user.js
@@ -1,21 +1,25 @@
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
-      this.belongsToMany(models.Product, { through: models.Basket, foreignKey: 'usesBas_id' });
+      this.belongsToMany(models.Product, {
+        through: models.Basket,
+        foreignKey: 'usesBas_id',
+      });
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    age: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      age: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
   return User;
 };
