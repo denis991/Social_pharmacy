a590a8e4c4178b71854f4f7fe59ad72c24e18a47
Denis Gutsulak <d.99113@gmail.com>
2022-06-24 15:08:45 +0300

    npx sequelize db:migrate —-env production

--- db/database.js
+++ db/database.js
@@ -22,8 +22,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false },
-    },
+
   },
 };
